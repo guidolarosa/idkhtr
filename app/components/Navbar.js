f801a00c917607e8bec3c848ccde33791335d3f3
Guido La Rosa <guido.glarosa@gmail.com>
2023-12-01 11:16:03 -0300

    Updated site layout

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbar = ({selectedIssue, setShowSidebar, showSidebar}) => {
   return (
-    <div className="navbar fixed py-4 px-8 z-20 w-full flex top-0 left-0 justify-between">
-        <div>{selectedIssue && selectedIssue.title}</div>
+    <div className="navbar fixed py-4 px-8 z-20 w-full flex top-0 left-0 justify-between ">
+        <div className="ml-auto">{selectedIssue && selectedIssue.title}</div>
         {/* <div
           onClick={() => {
             setShowSidebar(!showSidebar);
